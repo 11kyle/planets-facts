4bb9f572e8b55bfdfe540524961ae2f4da8cbb28
11kyle <11kyle.johnson@gmail.com>
2022-12-07 15:00:05 -0600

    fully dynamic except mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,102 @@
+import { useState } from 'react';
 import './App.css';
 import { Tab } from './components/tab';
 import { Statistic } from './components/statistic';
 import mercury from './assets/planet-mercury.svg';
 
+import planetData from './data.json';
+
+
 function App() {
+  const [activePlanet, setActivePlanet] = useState('mercury');
+  const [activeTab, setActiveTab] = useState('overview');
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState('false');
+  console.log(planetData);
+
+  function toggleMobileMenu() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  function handleActivePlanet(planet) {
+    setActivePlanet(planet);
+  }
+
+  function handleActiveTab(tab) {
+    setActiveTab(tab);
+  }
+
   return (
     <div>
       <header>
         <nav className="flex flex-row items-center justify-between flex-wrap border-b border-[#38384F] px-6 py-4">
           <h1 className="uppercase text-white text-[28px] leading-[36.23px] -tracking-[1.05px] font-antonio font-medium md:mx-auto lg:mx-0 md:mb-10 lg:mb-0">The Planets</h1>
-          <button className="md:hidden">
+          <button className="md:hidden" onClick={toggleMobileMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fillRule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g></svg>
           </button>
-          <ul className="hidden md:flex flex-row gap-x-8 md:basis-full lg:basis-0 justify-center">
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Mercury</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Venus</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Earth</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Mars</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Jupiter</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Saturn</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Uranus</li>
-            <li className="heading-sm text-[#D8D8D8] cursor-pointer">Neptune</li>
+          <ul className="hidden md:flex flex-row gap-x-8 md:basis-full lg:basis-0 md:justify-center">
+            {planetData.map((planet) => (
+              <li className="heading-sm text-[#D8D8D8] cursor-pointer" onClick={() => handleActivePlanet(planet.name.toLowerCase())} key={planet.name}>{planet.name}</li>
+            ))}
           </ul>
         </nav>
       </header>
-      <main className="grid grid-cols-2 gap-x-[70px] md:max-w-[689px] lg:max-w-[1110px] mx-auto">
 
-        {/* Tab Bar */}
-        <div className="md:order-3 col-span-2 md:col-span-1 flex flex-row md:flex-col justify-between md:justify-center md:items-end md:gap-y-4 border-b md:border-none border-[#38384F] px-6 md:px-0">
-          <Tab><span className="hidden md:block text-[#D8D8D8] ml-5 mr-4">01</span><span>Overview</span></Tab>
-          <Tab><span className="hidden md:block text-[#D8D8D8] ml-5 mr-4">02</span><span>Structure</span></Tab>
-          <Tab><span className="hidden md:block text-[#D8D8D8] ml-5 mr-4">03</span><span>Surface</span></Tab>
-        </div>
+      {planetData.filter((query) => query.name.toLowerCase() === activePlanet).map((planet) => (
+        <main className="grid grid-cols-2 gap-x-[70px] md:max-w-[689px] lg:max-w-[1110px] mx-auto" key={planet.name}>
 
-        {/* Planet Image */}
-        {/* 
-          mercury:  mobile: 111x111, tablet:  184x184,  desktop:  290x290
-          venus:    mobile: 154x154, tablet:  253x253,  desktop:  400x400
-          earth:    mobile: 173x173, tablet:  285x285,  desktop:  450x450
-          mars:     mobile: 129x129, tablet:  213x213,  desktop:  336x336
-          jupiter:  mobile: 224x224, tablet:  369x369,  desktop:  260x260
-          saturn:   mobile: 256x256, tablet:  422x422,  desktop:  666x666
-          uranus:   mobile: 176x176, tablet:  290x290,  desktop:  458x458
-          neptune:  mobile: 173x173, tablet:  285x285,  desktop:  450x450
-        */}
-        <div className="md:order-1 col-span-2 lg:col-span-1 lg:row-span-2 w-full h-[304px] md:h-[460px] lg:h-[754px] flex flex-row justify-center">
-          <img className="w-[111px] h-[111px] md:w-[184px] md:h-[184px] lg:w-[290px] lg:h-[290px] my-auto" src={mercury} />
-        </div>
+          {/* Tab Bar */}
+          <div className="md:order-3 col-span-2 md:col-span-1 flex flex-row md:flex-col justify-between md:justify-center md:items-end md:gap-y-4 border-b md:border-none border-[#38384F] px-6 md:px-0">
+            {["overview", "structure", "geology"].map((tab, index) => (
+              <div className={`${activeTab === tab && activePlanet} heading-md btn flex flex-row justify-center md:justify-start cursor-pointer`} key={tab} onClick={() => handleActiveTab(tab)}>
+                <span className="hidden md:block text-[#D8D8D8] ml-5 mr-4">0{index + 1}</span><span>{tab}</span>
+              </div>
+              // <Tab key={tab}><span className="hidden md:block text-[#D8D8D8] ml-5 mr-4">0{index + 1}</span><span>{tab}</span></Tab>
+            ))}
+          </div>
 
-        {/* Description */}
-        <div className="md:order-2 col-span-2 md:col-span-1 lg:self-end text-center md:text-left mx-6 md:mx-0">
-          <h1 className="heading-xl">Mercury</h1>
-          <p className="body mb-8">Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.</p>
-          <p className="body">Source : Wikipedia</p>
-        </div>
+          {/* Planet Image */}
+          {/* 
+            mercury:  mobile: 111x111, tablet:  184x184,  desktop:  290x290
+            venus:    mobile: 154x154, tablet:  253x253,  desktop:  400x400
+            earth:    mobile: 173x173, tablet:  285x285,  desktop:  450x450
+            mars:     mobile: 129x129, tablet:  213x213,  desktop:  336x336
+            jupiter:  mobile: 224x224, tablet:  369x369,  desktop:  260x260
+            saturn:   mobile: 256x256, tablet:  422x422,  desktop:  666x666
+            uranus:   mobile: 176x176, tablet:  290x290,  desktop:  458x458
+            neptune:  mobile: 173x173, tablet:  285x285,  desktop:  450x450
+          */}
+          <div className="md:order-1 col-span-2 lg:col-span-1 lg:row-span-2 w-full h-[304px] md:h-[460px] lg:h-[754px] flex flex-row justify-center">
+            <img className="w-[111px] h-[111px] md:w-[184px] md:h-[184px] lg:w-[290px] lg:h-[290px] my-auto" src={mercury} />
+          </div>
 
-        {/* Statistics */}
-        <div className="order-4 col-span-2 flex flex-col md:flex-row md:justify-between gap-y-2 md:gap-x-2 mx-6 md:mx-0 mt-7 lg:mt-20 md:mb-9">
-          <Statistic 
-            data="58.6 Days"
-            subject="Rotation Time"
-          />
-          <Statistic 
-            data="87.97 Days"
-            subject="Revolution Time"
-          />
-          <Statistic 
-            data="2,439.7 km"
-            subject="Radius"
-          />
-          <Statistic 
-            data="430 C"
-            subject="Average Temp."
-          />
-        </div>
-      </main>
+          {/* Description */}
+          <div className="md:order-2 col-span-2 md:col-span-1 lg:self-end text-center md:text-left mx-6 md:mx-0">
+            <h1 className="heading-xl">{planet.name}</h1>
+            <p className="body mb-8">{planet[activeTab].content}</p>
+            <p className="body">Source : Wikipedia</p>
+          </div>
+
+          {/* Statistics */}
+          <div className="order-4 col-span-2 flex flex-col md:flex-row md:justify-between gap-y-2 md:gap-x-2 mx-6 md:mx-0 mt-7 lg:mt-20 md:mb-9">
+            <Statistic 
+              data={planet.rotation}
+              subject="Rotation Time"
+            />
+            <Statistic 
+              data={planet.revolution}
+              subject="Revolution Time"
+            />
+            <Statistic 
+              data={planet.radius}
+              subject="Radius"
+            />
+            <Statistic 
+              data={planet.temperature}
+              subject="Average Temp."
+            />
+          </div>
+        </main>
+      ))}
     </div>
   )
 }
